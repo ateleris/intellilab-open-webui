c48c510137a3e664a491516e1e66048e33061ffe
Luca Schafroth <luca.schafroth@ateleris.ch>
2025-02-18 10:04:15 +0100

    Increase splash size

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -175,14 +175,14 @@
 	<div class="w-full absolute top-0 left-0 right-0 h-8 drag-region" />
 
 	{#if loaded}
-		<div class="fixed m-10 z-50">
+		<div class="fixed z-50">
 			<div class="flex space-x-2">
 				<div class=" self-center">
 					<img
 						id="logo"
 						crossorigin="anonymous"
 						src="{WEBUI_BASE_URL}/static/splash.png"
-						class=" w-6 rounded-full"
+						class="w-40 rounded-full dark:invert"
 						alt="logo"
 					/>
 				</div>
