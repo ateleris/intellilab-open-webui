cbf24469213c628a1340c884b6942b4c0e81853a
Luca Schafroth <luca.schafroth@ateleris.ch>
2025-02-21 17:12:10 +0100

    Change name to IntelliChat

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -1215,9 +1215,9 @@ async def oauth_callback(provider: str, request: Request, response: Response):
 @app.get("/manifest.json")
 async def get_manifest_json():
     return {
-        "name": WEBUI_NAME,
-        "short_name": WEBUI_NAME,
-        "description": "Open WebUI is an open, extensible, user-friendly interface for AI that adapts to your workflow.",
+        "name": app.state.WEBUI_NAME,
+        "short_name": app.state.WEBUI_NAME,
+        "description": "IntelliChat is an open, extensible, user-friendly interface for AI that adapts to your workflow.",
         "start_url": "/",
         "display": "standalone",
         "background_color": "#343541",
