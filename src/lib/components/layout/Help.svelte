19604bd66bc1429e6ef961c0b8976c6de92a24c2
Denis Müller <denis.mueller@synerma.ch>
2025-03-27 15:09:09 +0100

    re-style help button and point it to intellilab website

--- src/lib/components/layout/Help.svelte
+++ src/lib/components/layout/Help.svelte
@@ -11,30 +11,14 @@
 </script>
 
 <div class=" hidden lg:flex fixed bottom-0 right-0 px-1 py-1 z-20">
-	<button
-		id="show-shortcuts-button"
-		class="hidden"
-		on:click={() => {
-			showShortcuts = !showShortcuts;
-		}}
-	/>
-
-	<HelpMenu
-		showDocsHandler={() => {
-			showShortcuts = !showShortcuts;
-		}}
-		showShortcutsHandler={() => {
-			showShortcuts = !showShortcuts;
-		}}
-	>
-		<Tooltip content={$i18n.t('Help')} placement="left">
-			<button
-				class="text-gray-600 dark:text-gray-300 bg-gray-300/20 size-4 flex items-center justify-center text-[0.7rem] rounded-full"
+	
+			<a
+				class="text-[#00e8ba] w-8 bg-[#003832] font-extrabold flex items-center justify-center text-[1.2rem] rounded-full cursor-pointer mr-2 mb-2"
+				href="https://www.intellilab.ch/how-to-intellichat"
+				target="_blank"
 			>
 				?
-			</button>
-		</Tooltip>
-	</HelpMenu>
+			</a>
 </div>
 
 <ShortcutsModal bind:show={showShortcuts} />
